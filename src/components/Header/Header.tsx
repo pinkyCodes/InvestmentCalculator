2d33dd8ae0f0df5eebdc378c91c4b991322c2574
Bilyana Mancheva <bilyana.mancheva1991@gmail.com>
2023-09-05 19:20:58 +0300

    style: change logo img

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import logo from '../../assets/investment-calculator-logo.png';
+import logo from '../../assets/scrooge-mcduck.png';
 import classes from './Header.module.css';
 
 const Header = (): JSX.Element => {
